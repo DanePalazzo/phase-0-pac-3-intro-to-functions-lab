b679f8615c51bd64e8ce336eee21a0249df88eed
Dane Palazzo <PalazzoDD@comcast.net>
2023-08-24 14:47:08 -0600

    phase-0-pac-3-intro-to-functions-lab completed

---
+++ index.js
@@ -0,0 +1,42 @@
+const { spyOn } = require("expect")
+
+function shout(string) {
+    return string.toUpperCase ();
+}
+
+function whisper(string) {
+    return string.toLowerCase ();
+}
+
+function logShout(string) {
+    console.log (shout(string))
+}
+
+function logWhisper(string) {
+    console.log (whisper(string))
+}
+
+/*
+function sayHiToHeadphonedRoommate(string) {
+    if(string === string.toLowerCase()){
+        return "I can\'t hear you!"
+    } else if(string === string.toUpperCase()){
+        return "YES INDEED!"
+    } else if(string === "Let\'s have dinner together!"){
+        return "I would love to!"
+    }
+}
+*/
+
+function sayHiToHeadphonedRoommate(string) {
+    if(string === string.toUpperCase()){
+        return "YES INDEED!"
+    } else if(string === "Let\'s have dinner together!"){
+        return "I would love to!"
+    } else{
+        return "I can\'t hear you!"
+    }
+
+}
+
+console.log(sayHiToHeadphonedRoommate("Hello"))
